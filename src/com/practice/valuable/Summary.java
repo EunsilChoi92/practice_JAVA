0de78c2c8a53d8a15d2debb5ced6a16588e39a0d
EunsilChoi92 <atheung1@naver.com>
2024-01-11 20:41:32 +0900

    Initial commit

---
+++ src/com/practice/valuable/Summary.java
@@ -0,0 +1,19 @@
+package com.practice.valuable;
+
+public class Summary {
+    public static void main(String[] args) {
+        //주석 쓰는 법
+        //한 줄씩 쓰는 법: //(슬래시 두개)
+        //여러 줄씩 쓰는 법: ctrl + shift + /
+        /* <- 주석의 시작
+        * 이렇게 엔터 치면
+        * 여러 줄 알아서
+        * 주석 처리가 된다
+        주석의 끝 -> */
+
+        //인텔리제이는 자동 저장이 된다.
+        //하지만 ctrl+S 누르는 걸 생활화하자!!!
+
+
+    }
+}
